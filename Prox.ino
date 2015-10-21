939ae208acb0d14ee4c1b89b95455b34df9f2125
J0hnnyLee <JohnESummers.1@gmail.com>
2015-10-21 17:54:16 -0600

    Updated for indicators and HC-SR04 sensor.

--- Prox.ino
+++ Prox.ino
@@ -1,54 +1,89 @@
-#include <Wire.h>
-
-int ledPin = 13;                 // LED connected to digital pin 13
-int ledPin = 14;                 // LED connected to digital pin 14
-int ledPin = 15;                 // LED connected to digital pin 15
+// HC-SR04 echo and trigger pin definitions
+#define TRIGGER 13
+#define ECHO 12
+// Units divisor - 58.0 is millimeters, 1473.2 is inches
+#define UNITS_DIVISOR 1473.2
+// Ping setup for distance measurements
+#define DELAY_BEFORE_NEW_PING 5
+#define NUMBER_OF_PINGS 25
+#define DELAY_BETWEEN_GROUPS 250
+#define TRIGGER_LENGTH 11
+// LED output pin definitions
+#define RED_LED 14
+#define GREEN_LED 15
+#define BLUE_LED 16
+// Inch distances for indicators, red is on for anything less than BLUE_ON.
+// BLUE_ON is the closest edge of the sweet spot, GREEN_ON is the farthest edge.
+#define ALL_OFF 96
+#define GREEN_ON 36
+#define BLUE_ON 32
 
 void setup() {
-  Wire.begin();                // join i2c bus (address optional for master)
-  Serial.begin(9600);          // start serial communication at 9600bps
-  pinMode(13, OUTPUT);         // initialize digital pin 13, 14, 15 as an output.
-  pinMode(14, OUTPUT);
-  pinMode(15, OUTPUT);
+   // Set up to send output to console
+   Serial.begin(9600);
+
+   // Initialiaze the I/O
+   // LEDs
+   pinMode(RED_LED, OUTPUT);
+   pinMode(GREEN_LED, OUTPUT);
+   pinMode(BLUE_LED, OUTPUT);
+   // The HC-SR04 sensor
+   pinMode(TRIGGER, OUTPUT);
+   pinMode(ECHO, INPUT);
+
+   // Reset the trigger pin and wait a half a second
+   digitalWrite(TRIGGER, LOW);
+   delayMicroseconds(500);
 }
 
-int reading = 0;
-
 void loop() {
-  // step 1: instruct sensor to read echoes
-  Wire.beginTransmission(112); // transmit to device #112 (0x70)   
-                               // the address specified in the datasheet is 224 (0xE0)
-                               // but i2c adressing uses the high 7 bits so it's 112
-  Wire.write(byte(0x00));      // sets register pointer to the command register (0x00)
-  Wire.write(byte(0x50));      // command sensor to measure in "inches" (0x50)
-                               // use 0x51 for centimeters
-                               // use 0x52 for ping microseconds
-  Wire.endTransmission();      // stop transmitting
-
-  // step 2: wait for readings to happen
-  delay(70);                   // datasheet suggests at least 65 milliseconds
-
-  // step 3: instruct sensor to return a particular echo reading
-  Wire.beginTransmission(112); // transmit to device #112
-  Wire.write(byte(0x02));      // sets register pointer to echo #1 register (0x02)
-  Wire.endTransmission();      // stop transmitting
-
-  // step 4: request reading from sensor
-  Wire.requestFrom(112, 2);    // request 2 bytes from slave device #112
-
-  // step 5: receive reading from sensor
-  if (2 <= Wire.available()) { // if two bytes were received
-    reading = Wire.read();     // receive high byte (overwrites previous reading)
-    reading = reading << 8;    // shift high byte to be high 8 bits
-    reading |= Wire.read();    // receive low byte as lower 8 bits
-    Serial.println(reading);   // print the reading
+  delay(DELAY_BETWEEN_GROUPS);
+  long measurement = groupPing();
+  // Print out distance in inches
+  Serial.print("Distance: ");
+  Serial.print(measurement);
+  Serial.println(" inches");
+  // Set the indicator
+  if(measurement > ALL_OFF) {
+    // Greater than 96 inches, no indicator
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+  } else if(measurement > GREEN_ON) {
+    // Greater than 36 and less than 96, green indicator
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(RED_LED, LOW);
+  } else if(measurement > BLUE_ON) {
+    // Greater that 32 and less than 36 - sweet spot, blue indicator
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(BLUE_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+  } else {
+    // Less than 32 inches, red indicator
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
   }
-  // Writing to the pins turning them on and off. needs work
-  digitalWrite(13, HIGH);     // turn the LED on (HIGH is the voltage level)
-  delay(1000);                // wait for a second
-  digitalWrite(13, LOW);      // turn the LED off by making the voltage LOW
-  delay(1000);                // wait for a second
+}
 
+long groupPing() {
+  long sum = 0;
+  for (int i = 0; i < NUMBER_OF_PINGS; i++)
+  {
+    delay(DELAY_BEFORE_NEW_PING);
+    sum += ping();
+  }
+  return sum / NUMBER_OF_PINGS;
+}
 
-  delay(250);                  // wait a bit since people have to read the output :)
+long ping() {
+  // Trigger - raise the trigger for TRIGGER_LENGTH, then set it back low
+  digitalWrite(TRIGGER, HIGH);
+  delayMicroseconds(TRIGGER_LENGTH);
+  digitalWrite(TRIGGER, LOW);
+  // Get the echo length - wonder if this can actually start before lowering
+  // the trigger line...
+  int pulseWidth = pulseIn(ECHO, HIGH);
+  return (long) (((float) pulseWidth / UNITS_DIVISOR) * 10.0);
 }
